213f1e20d19cc24675414cd6a12b8e1b5325e65c
SHOBHIT NAIR <84396150+ShobhitNair@users.noreply.github.com>
2024-03-02 12:31:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,9 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.resolve(__dirname,"/frontend/build")));
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+})
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
